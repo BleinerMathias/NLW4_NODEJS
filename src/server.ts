86759639ad8c7904a75f58fd06a02ab507475913
Bleiner Mathias <bleinerbbm@hotmail.com>
2021-02-24 11:51:44 -0300

    Criando base de dados
    2º dia: Criando base de dados SQLITE, e entidade usuários

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,13 @@
+import 'reflect-metadata';
 import express from 'express';
+import './database';
+import { router } from './routes';
 
 const app = express();
 
-app.get("/", (req, res) => {
-    return res.json({ message: "Hello World!" });
-})
+app.use(express.json());
+app.use(router);
 
-app.post("/user", (req, res) => {
-    res.json({ message: "Dados enviados com sucesso" });
-})
 
 
 app.listen(3333, () => console.log("Server is running!"));
